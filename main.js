639b23b2a24dfcaae3c2f39d0a63befb197a702f
skw622 <skw622vlato@gmail.com>
2019-10-25 14:13:27 +0300

    new figure

--- main.js
+++ main.js
@@ -7,39 +7,39 @@ const path = require('path')
 let mainWindow
 
 function createWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 600,
-    frame: false,
-    movable: true,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 600,
+        frame: true,
+        movable: true,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('./page/index.html')
+    // and load the index.html of the app.
+    mainWindow.loadFile('./page/index.html')
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools()
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    console.log('window closed', '')
-    mainWindow = null
-  })
+    // Emitted when the window is closed.
+    mainWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        console.log('window closed', '')
+        mainWindow = null
+    })
 
-  globalShortcut.register('CommandOrControl+1', () => {
-    mainWindow.isMaximized() ? mainWindow.unmaximize() : mainWindow.maximize()
-  });
+    globalShortcut.register('CommandOrControl+1', () => {
+        mainWindow.isMaximized() ? mainWindow.unmaximize() : mainWindow.maximize()
+    });
 
-  globalShortcut.register('Alt+1', () =>{
-    alert('alt key pressed')
-  })
+    globalShortcut.register('Alt+1', () => {
+        alert('alt key pressed')
+    })
 }
 
 // This method will be called when Electron has finished
@@ -48,16 +48,16 @@ function createWindow() {
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
+app.on('window-all-closed', function() {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) createWindow()
 })
 
 // In this file you can include the rest of your app's specific main process
--- preload.js
+++ preload.js
@@ -1,56 +1,87 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-  const { remote } = require('electron');
-  // const fetch = require('electron-fetch');
-  // let request = new ClientRequest();
-  // const request = net.request({
-  //   method: 'GET',
-  //   protocol: 'https:',
-  //   hostname: 'github.com',
-  //   port: 443,
-  //   path: '/'
-  // })
+    window.$ = window.jQuery = require('jquery')
+    const { remote } = require('electron');
+    let exePath = "./syncthing/syncthing.exe"
+    let child = require('child_process').execFile;
+    const axios = require('axios')
 
-  const axios = require('axios')
-
-  document.getElementById('close').addEventListener('click', closeWindow);
-  document.getElementById('minimize').addEventListener('click', minimizeWindow);
-  document.getElementById('max').addEventListener('click', maximizeWindow);
-  document.getElementById('fetch_data').addEventListener('click', fetchData);
-  document.getElementById('url').addEventListener('keyup', (event) => {
-    if (event.keyCode === 13) {
-      event.preventDefault();
-      document.getElementById("fetch_data").click();
-    }
-  })
-  function closeWindow() {
-    let window = remote.getCurrentWindow()
-    window.close()
-  }
-
-  function maximizeWindow() {
-    let window = remote.getCurrentWindow()
-    window.isMaximized() ? window.unmaximize() : window.maximize()
-  }
-
-  function minimizeWindow() {
-    let window = remote.getCurrentWindow()
-    window.minimize()
-  }
-
-
-  function fetchData() {
-    let api_key = document.getElementById('api_key').value;
-    let url = document.getElementById('url').value;
-
-    axios.defaults.headers = {
-      'X-API-Key': api_key
-    }
-    axios.get('http://127.0.0.1:8384' + url).then(res => {
-      document.getElementById('preview').innerHTML = `<pre>` + `${(JSON.stringify(res.data, null, "\t"))}` + `</pre>`
+    document.getElementById('close').addEventListener('click', closeWindow);
+    document.getElementById('minimize').addEventListener('click', minimizeWindow);
+    document.getElementById('max').addEventListener('click', maximizeWindow);
+    document.getElementById('fetch_data').addEventListener('click', fetchData);
+    document.getElementById('url').addEventListener('keyup', (event) => {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById("fetch_data").click();
+        }
     })
-  }
+
+    function closeWindow() {
+        let window = remote.getCurrentWindow()
+
+
+        let api_key = document.getElementById('api_key').value;
+        let url = 'rest/system/shutdown';
+
+        axios.defaults.headers = {
+            'X-API-Key': api_key
+        }
+        axios.post('http://127.0.0.1:8384/' + url).then(res => {
+            document.getElementById('preview').innerHTML = `<pre>` + `${(JSON.stringify(res.data, null, "\t"))}` + `</pre>`
+            setTimeout(() => {
+                window.close()
+            }, 1000);
+        })
+    }
+
+    function maximizeWindow() {
+        let window = remote.getCurrentWindow()
+        window.isMaximized() ? window.unmaximize() : window.maximize()
+    }
+
+    function minimizeWindow() {
+        let window = remote.getCurrentWindow()
+        window.minimize()
+    }
+
+    child(exePath, function(err, data) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        console.log(data.toString());
+    });
+
+    async function fetchData() {
+        let response = await axios.get('http://127.0.0.1:8384/').then(res => {
+            return res.headers
+        })
+        console.log(response)
+        let cookie = response['set-cookie'][0]
+        let csrf_token_header = cookie.split('=')[0]
+        let csrf_token = cookie.split('=')[1]
+        let method = $("#method option:selected").text();
+        let url = document.getElementById('url').value;
+        console.log(cookie, csrf_token)
+        axios.defaults.headers = {
+            'Cookie': cookie,
+        }
+        axios.defaults.headers[csrf_token_header] = csrf_token
+        if (method == 'GET') {
+            axios.get('http://127.0.0.1:8384/' + url).then(res => {
+                document.getElementById('preview').innerHTML = `<pre>` + `${(JSON.stringify(res.data, null, "\t"))}` + `</pre>`
+            })
+        }
+        if (method == 'POST') {
+            axios.post('http://127.0.0.1:8384/' + url).then(res => {
+                document.getElementById('preview').innerHTML = `<pre>` + `${(JSON.stringify(res.data, null, "\t"))}` + `</pre>`
+            })
+        }
+
+    }
 })
 
 
